b0d57a63cac52a7ee680dbeda3b2b95e611a0612
Gustavo Rabusky <gustavor1@unisc.br>
2023-01-30 15:41:18 -0300

    Ajustes finais

--- src.js
+++ src.js
@@ -1,6 +1,12 @@
 define([], function () {
   return {
-    calculaDiferencaEntreDias: function (dataInicial, dataFinal) {
+    getDiffDias: function (dataInicial, dataFinal) {
+      if (dataFinal < dataInicial) {
+        let dataAuxiliar = dataInicial;
+        dataInicial = dataFinal;
+        dataFinal = dataAuxiliar;
+      }
+
       let diaInicio = this.pegaDia(dataInicial);
       let diaFinal = this.pegaDia(dataFinal);
       let mesInicio = this.pegaMes(dataInicial);
@@ -8,12 +14,13 @@ define([], function () {
       let anoInicial = this.pegaAno(dataInicial);
       let anoFinal = this.pegaAno(dataFinal);
 
-      if (anoInicial == anoFinal) {
-        if (mesInicio == mesFinal) {
-          dias = diaInicio - diaFinal;
-          console.log(dias);
-        }
-      }
+      let dias =
+        this.diferencaDiasEntreAnos(anoInicial, anoFinal) +
+        (this.diasEntreMes(mesFinal, anoFinal) -
+          this.diasEntreMes(mesInicio, anoInicial)) +
+        (diaFinal - diaInicio);
+
+      return dias;
     },
     pegaDia: function (date) {
       let dia = date.split("-");
@@ -28,16 +35,17 @@ define([], function () {
       return Number(ano[0]);
     },
     diferencaDiasEntreAnos: function (anoInicial, anoFinal) {
-      dias = 0;
-      diasDoAno = 365;
-      for (let ano = anoInicial + 1; ano < anoFinal; ano++) {
+      let dias = 0;
+      let diasDoAno = 365;
+      for (let ano = anoInicial; ano < anoFinal; ano++) {
         diasDoAno = 365;
         if (this.anoBissexto(ano)) {
           diasDoAno = 366;
         }
         dias += diasDoAno;
-        console.log(dias);
       }
+
+      return dias;
     },
     anoBissexto: function (ano) {
       if (ano % 400 === 0 || (ano % 100 !== 0 && ano % 4 === 0)) {
@@ -47,12 +55,11 @@ define([], function () {
     },
 
     diasEntreMes: function (mes, ano) {
-      diasMes = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-      if (mes == 2 && this.anoBissexto(ano) == true) {
-        return 29;
-      } else {
-        return diasMes[mes];
+      diasMes = [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+      if (this.anoBissexto(ano)) {
+        diasMes = [0, 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335];
       }
+      return diasMes[mes];
     },
   };
 });
--- test.js
+++ test.js
@@ -5,13 +5,55 @@ require(["./src.js"], function (src) {
   });
 
   QUnit.module("Contando Dias");
-  QUnit.test("Test retorno dia", function (assert) {
-    assert.equal(src.calculaDiferencaEntreDias("2023-01-27", "2023-01-22"), 5);
+  QUnit.module("Calculando diferença dias - o teste principal");
+  QUnit.test("Teste #1 - duas datas no mesmo mês", function (assert) {
+    assert.equal(src.getDiffDias("2023-01-26", "2023-01-27"), 1);
   });
-  QUnit.test("Test retorno dia", function (assert) {
-    assert.equal(src.diferencaDiasEntreAnos(2011, 2014), 1);
+  QUnit.test("Teste #2 - duas datas em meses subsequentes", function (assert) {
+    assert.equal(src.getDiffDias("2023-01-26", "2023-02-20"), 25);
   });
-  QUnit.test("Test retorno dia", function (assert) {
-    assert.equal(src.diasEntreMes(02, 2000), 29);
+  QUnit.test(
+    "Teste #3 - datas em meses subsequentes - fevereiro de ano não bissexto",
+    function (assert) {
+      assert.equal(src.getDiffDias("2023-02-26", "2023-03-20"), 22);
+    }
+  );
+  QUnit.test(
+    "Teste #4 - datas em meses subsequentes - fevereiro de ano bissexto",
+    function (assert) {
+      assert.equal(src.getDiffDias("2024-02-26", "2024-03-20"), 23);
+    }
+  );
+  QUnit.test("Teste #5 - datas em intervalos de meses", function (assert) {
+    assert.equal(src.getDiffDias("2023-03-01", "2023-09-20"), 203);
   });
+
+  QUnit.test(
+    "Teste #6 - datas em intervalos de meses em ano bissexto",
+    function (assert) {
+      assert.equal(src.getDiffDias("2024-01-01", "2024-12-31"), 365);
+    }
+  );
+
+  QUnit.test("Teste #7 - datas em ano subsequente", function (assert) {
+    assert.equal(src.getDiffDias("2023-12-15", "2024-01-15"), 31);
+  });
+
+  QUnit.test("Teste #8 - datas em ano diferentes", function (assert) {
+    assert.equal(src.getDiffDias("2020-08-30", "2024-01-15"), 1233);
+  });
+
+  QUnit.test(
+    "Teste #9 - datas em ano diferentes, passando ano bissexto",
+    function (assert) {
+      assert.equal(src.getDiffDias("2019-08-10", "2024-09-01"), 1849);
+    }
+  );
+
+  QUnit.test(
+    "Teste #10 - número de dias deste o dia 01/01/1970",
+    function (assert) {
+      assert.equal(src.getDiffDias("2023-01-27", "1970-01-01"), 19384);
+    }
+  );
 });
